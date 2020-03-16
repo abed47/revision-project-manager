c56fba74ee84b4ab54402f6ffe922cbad4bdf42d
abdull-rahman <abed472011@gmail.com>
2020-03-16 23:38:53 +0200

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA2pn-i5hOI-H2UamNZhC5cnyXapKuCyUQ",
+    authDomain: "revision-project-manager.firebaseapp.com",
+    databaseURL: "https://revision-project-manager.firebaseio.com",
+    projectId: "revision-project-manager",
+    storageBucket: "revision-project-manager.appspot.com",
+    messagingSenderId: "878065775503",
+    appId: "1:878065775503:web:05d8910c3c52e27879d1fa",
+    measurementId: "G-3CCJ2XK5PS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();

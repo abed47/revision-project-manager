1836227bff9cd795b0b0016e0080f7b306a26ca5
abdull-rahman <abed472011@gmail.com>
2020-03-17 03:49:11 +0200

    added project sidebar styling

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -4,7 +4,7 @@ import { FaChevronDown, FaInbox, FaRegCalendar, FaRegCalendarAlt } from 'react-i
 export const Sidebar = () => {
     return (
         <div className="sidebar" data-testid="sidebar">
-            <ul className="sidebar_generic">
+            <ul className="sidebar__generic">
                 <li data-testid="inbox" className="inbox">
                     <span><FaInbox/></span>
                     <span>Inbox</span>
@@ -18,11 +18,11 @@ export const Sidebar = () => {
                     <span>Next 7 days</span>
                 </li>
             </ul>
-            <div className="sidebar_middle">
+            <div className="sidebar__middle">
                 <span><FaChevronDown/></span>
                 <h2>Projects</h2>
             </div>
-            <ul className="sidebar_projects">
+            <ul className="sidebar__projects">
                 Projects will be here
             </ul>
             Add projects here
